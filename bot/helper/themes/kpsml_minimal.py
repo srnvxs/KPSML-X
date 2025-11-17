6b06694c84361895103269ad6e74e47177161290
venkata srinivas <104828717+srnvxs@users.noreply.github.com>
2025-11-17 17:38:38 +0530

    Update kpsml_minimal.py

--- bot/helper/themes/kpsml_minimal.py
+++ bot/helper/themes/kpsml_minimal.py
@@ -2,10 +2,10 @@
 class KPSMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = '🤖 Owner'
+    ST_BN1_NAME = '🤖 Owner '
     ST_BN1_URL = 'https://telegram.me/SriPspk0'
     ST_BN2_NAME = 'Updates 🔥'
-    ST_BN2_URL = 'https://telegram.me/Kpspkbots'
+    ST_BN2_URL = 'https://telegram.me/pspkbots'
     ST_MSG = '''<b><i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>\n
 Type {help_command} to get a list of available commands</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
