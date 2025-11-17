812bc79bff4ab58ebb7931bcaa0b77d9ce9212c0
venkata srinivas <104828717+srnvxs@users.noreply.github.com>
2025-11-17 17:01:57 +0530

    Update bot_settings.py

--- bot/modules/bot_settings.py
+++ bot/modules/bot_settings.py
@@ -42,10 +42,10 @@ default_values = {'AUTO_DELETE_MESSAGE_DURATION': 30,
                   'BOT_THEME': 'minimal',
                   'BOT_LANG': 'en',
                   'IMG_PAGE': 1,
-                  'AUTHOR_NAME': 'KPS Bots',
-                  'AUTHOR_URL': 'https://telegram.me/KPSBots',
-                  'TITLE_NAME': 'KPSML-X Leech Bot',
-                  'GD_INFO': 'Uploaded by KPSML-X Leech Bot',
+                  'AUTHOR_NAME': 'PSPK Bots',
+                  'AUTHOR_URL': 'https://telegram.me/PSPKBots',
+                  'TITLE_NAME': 'PSPK Leech Bot',
+                  'GD_INFO': 'Uploaded by PSPK Leech Bot',
                   }
 bool_vars = ['AS_DOCUMENT', 'BOT_PM', 'STOP_DUPLICATE', 'SET_COMMANDS', 'SAVE_MSG', 'SHOW_MEDIAINFO', 'SOURCE_LINK', 'SAFE_MODE', 'SHOW_EXTRA_CMDS',
              'IS_TEAM_DRIVE', 'USE_SERVICE_ACCOUNTS', 'WEB_PINCODE', 'EQUAL_SPLITS', 'DISABLE_DRIVE_LINK', 'DELETE_LINKS', 'CLEAN_LOG_MSG', 'USER_TD_MODE', 
@@ -358,7 +358,7 @@ async def load_config():
 
     UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
     if len(UPSTREAM_REPO) == 0:
-        UPSTREAM_REPO = 'https://github.com/Tamilupdates/KPSML-X'
+        UPSTREAM_REPO = 'https://github.com/srnvxs/KPSML-X'
 
     UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
     if len(UPSTREAM_BRANCH) == 0:
@@ -450,23 +450,23 @@ async def load_config():
 
     AUTHOR_NAME = environ.get('AUTHOR_NAME', '')
     if len(AUTHOR_NAME) == 0:
-        AUTHOR_NAME = 'KPS Bots'
+        AUTHOR_NAME = 'PSPK Bots'
 
     AUTHOR_URL = environ.get('AUTHOR_URL', '')
     if len(AUTHOR_URL) == 0:
-        AUTHOR_URL = 'https://telegram.me/KPSBots'
+        AUTHOR_URL = 'https://telegram.me/KPSPBots'
 
     TITLE_NAME = environ.get('TITLE_NAME', '')
     if len(TITLE_NAME) == 0:
-        TITLE_NAME = 'KPSML-X Leech Bot'
+        TITLE_NAME = 'PSPK Leech Bot'
         
     COVER_IMAGE = environ.get('COVER_IMAGE', '')
     if len(COVER_IMAGE) == 0:
-        COVER_IMAGE = 'https://graph.org/file/0ff9d5e94a070fe4154c0.jpg'
+        COVER_IMAGE = 'https://envs.sh/VYO.jpg'
 
     GD_INFO = environ.get('GD_INFO', '')
     if len(GD_INFO) == 0:
-        GD_INFO = 'Uploaded by KPSML-X Leech Bot'
+        GD_INFO = 'Uploaded by PSPK Leech Bot'
 
     SAVE_MSG = environ.get('SAVE_MSG', '')
     SAVE_MSG = SAVE_MSG.lower() == 'true'
